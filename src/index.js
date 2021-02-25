79be1f3020fc045949291d319b6ddc1351253a94
Anubhav Sarkar <anubhavsarkar7@gmail.com>
2021-02-26 02:38:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function HelloWorld() {
+  return <h1>Hello World</h1>;
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
