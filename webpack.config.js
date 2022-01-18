54e8fed23277a4c94fd3be95da3aafdb4439d9b8
Elmer <5368806+elmerland@users.noreply.github.com>
2022-01-18 00:31:46 -0600

    Fix failing webserver config (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
   mode: process.env.NODE_ENV || "development",
   resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-  devServer: { contentBase: path.join(__dirname, "src") },
+  devServer: { static: { directory: path.join(__dirname, "src") } },
   module: {
     rules: [
       {
